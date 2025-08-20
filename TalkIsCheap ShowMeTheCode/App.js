a1c5bc0b8d7932f8dd5530f18c0829fa0273e8b5
RockeR26 <messithiago300@gmail.com>
2025-08-21 00:53:08 +0530

    Lesson 4 start

---
+++ TalkIsCheap ShowMeTheCode/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+
+* FoodApp
+
+* 1.Header
+* - Logo
+* - Nav Links
+
+* 2.Body
+* - Search
+* - Restaurant Container
+* - Restaurant Card
+
+* 3.Footer
+* - Copyright 
+* - Links
+* - Address 
+* - Contact 
+
+*/
+
+
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>)
