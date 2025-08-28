a9ed12c7ae66673d86222baac5ea0b62246c117a
RockeR26 <messithiago300@gmail.com>
2025-08-29 02:17:04 +0530

    Lesson 7 upddated

--- ep-7 Finding the Path/src/assets/link.js
+++ ep-7 Finding the Path/src/assets/link.js
@@ -1,5 +1,5 @@
 export const RES_IMAGE_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-export const URL="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const URL="https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5596952&lng=88.4096112&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 export const LOGO_URL="https://github.com/RockeR26/NamasteReact/blob/main/ep-6%20Explore%20the%20World/src/assets/logo.png?raw=true"
-export const URL_MENU="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9352403&lng=77.624532&restaurantId="
+export const URL_MENU="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5596952&lng=88.4096112&restaurantId="
 export const MENU_URL_IMG="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
