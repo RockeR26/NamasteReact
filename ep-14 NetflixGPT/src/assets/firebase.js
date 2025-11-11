e53b1e7ab22e8bc6bfc46abe9f36d1a64f1b6be8
RockeR26 <messithiago300@gmail.com>
2025-11-12 01:34:06 +0530

    netflixgpt auth 2

--- ep-14 NetflixGPT/src/assets/firebase.js
+++ ep-14 NetflixGPT/src/assets/firebase.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth"
+import { API_KEY } from "../../secrets";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCneisniVg2KADm1wA5v7ZkfP5kIlUSmJU",
+  apiKey:API_KEY,
   authDomain: "netflixgpt-4a60d.firebaseapp.com",
   projectId: "netflixgpt-4a60d",
   storageBucket: "netflixgpt-4a60d.firebasestorage.app",
